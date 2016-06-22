687784f084251c3dcc6765c53d4380c88643bf82
henockgm <heniboz@gmail.com>
2016-06-23 02:49:07 +0300

    edited css for responsive layout of posts

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,8 +3,13 @@
 @section('title') {{ $post->title }} @stop
 
 @section('content')
+
+
 	 <div class="container alt1">
+
+			<a href="{{ URL::route('home')}}"> back </a>
 		 <article class="make-center main-post-body-wrapper">
+
 		 	<h2> <a class="post-title post-title-main" href="{{ URL::action('post-show', $post->slug) }}">{{ $post->title }}</a> </h2> 
 		 		<div class="post-body-main"> {{ Markdown::parse($post->body) }}  </div> 		
 		</article>
