3fb6c74aad5e6eeb53888df54f45c29d4609a9eb
henockgm <heniboz@gmail.com>
2016-06-23 11:54:58 +0300

    edited css for responsive layout

--- app/views/edufiles/cv.blade.php
+++ app/views/edufiles/cv.blade.php
@@ -3,7 +3,7 @@
 @section('content')
  
     <div class="cv-holder">
-	<iframe class="pdf-holder" src="{{ asset('files/CV_HenockMullissa.pdf') }}" > </iframe>
+	<iframe class="pdf-holder" src="{{ asset('files/CV_HenockMullissa.pdf') }}"  frameBorder="0"> </iframe>
     </div>
       
 @stop
