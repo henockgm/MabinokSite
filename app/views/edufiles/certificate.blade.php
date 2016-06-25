a9258b708ef438030b8509314b854508e7f62de9
henockgm <heniboz@gmail.com>
2016-06-25 15:51:50 +0300

    edited css for posts responsive views

--- app/views/edufiles/certificate.blade.php
+++ app/views/edufiles/certificate.blade.php
@@ -2,9 +2,9 @@
 @section ('title') Certificate @stop
 @section('content')
  
-<div class="container"> 
+
     <div class="cv-holder" style="margin: 0 auto; width:90%; margin-right:50px;">
 		<iframe class="pdf-holder" src="{{ asset('files/certificate.pdf') }}"> </iframe>
     </div>
-</div>
+    
 @stop
--- app/views/edufiles/references.blade.php
+++ app/views/edufiles/references.blade.php
@@ -4,7 +4,7 @@
 
 
      
-    <div class="container project-container">
+    
       
     <div class="cv-holder">
         <h4 class="file-title"> Robert Äström and Johan Johansson - Raisoft Oy Ltd.  </h4>
@@ -15,5 +15,5 @@
          <h4 class="file-title"> Juha Nurmela - Finland Statistics Research Head  </h4>
 	   <iframe class="pdf-holder" src="{{ asset('files/ref_juhanurmela.pdf') }}"> </iframe>
     </div>
- </div>
+
 @stop
