9b048f143a5fec9a529a958dd6fd3f4e4844d3bb
henockgm <heniboz@gmail.com>
2017-01-10 18:34:07 +0200

    modifies error page view

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,3 +1,3 @@
-<?php 
-  return Redirect::route('home');
- ?>
+The requisted url is not found. Or the old URL is replaced to 
+
+ <a href="{{URL::route('home')}}" target="_blank">  mabinok.com </a>
