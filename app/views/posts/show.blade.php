040d8f35eaa491aa24062088f3c12d94189d4bae
henockgm <heniboz@gmail.com>
2017-02-21 20:51:48 +0200

    minor css edit

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -7,7 +7,7 @@
 
 	 <div class="container alt1">
 
-			<a class="back-btn" href="{{ URL::previous() }}"> <i class="fa fa-xlg fa-arrow-circle-left" aria-hidden="true" "></i> </a>
+			<a class="btn-orange-white" href="{{ URL::previous() }}"> <i class="fa fa-xlg fa-arrow-circle-left" aria-hidden="true" "></i> </a>
 
 
 		 <article class="main-post-body-wrapper">
